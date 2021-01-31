827a91f148b4e6126d4fe9c869025403c1c161aa
Eduardo Reinheimer <du428@hotmail.com>
2021-01-31 18:04:48 -0300

    First Import

---
+++ TypingGame/Main.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace TypingGame
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRight = new System.Windows.Forms.Label();
+            this.lblWrong = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Location = new System.Drawing.Point(25, 36);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(53, 13);
+            this.lblRight.TabIndex = 0;
+            this.lblRight.Text = "Correct: 0";
+            // 
+            // lblWrong
+            // 
+            this.lblWrong.AutoSize = true;
+            this.lblWrong.Location = new System.Drawing.Point(588, 36);
+            this.lblWrong.Name = "lblWrong";
+            this.lblWrong.Size = new System.Drawing.Size(61, 13);
+            this.lblWrong.TabIndex = 1;
+            this.lblWrong.Text = "Incorrect: 0";
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Tai Le", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(276, 81);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(122, 49);
+            this.lblWord.TabIndex = 2;
+            this.lblWord.Text = "Word";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(236, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Press Enter to Check the text with word";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(28, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(621, 55);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.checkGame);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 299);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblWrong);
+            this.Controls.Add(this.lblRight);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.Label lblWrong;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TypingGame/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypingGame
+{
+    public partial class Main : Form
+    {
+
+        string[] phrases = {"quero cafeh, isso aqui tah uma porcaria que nao tem merda nenhuma.",
+                            "vai todo mundo pra casa do caraleo.",
+                            "Foi otimo.",
+                            "Essa novinha keh pressaum."
+
+        };
+        Random rnd = new Random();
+        int correct = 0;
+        int incorrect = 0;
+        int index;
+        string currentPhrase = "";
+        List<string> words = new List<string> { };
+
+
+        public Main()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        public string GetPhrase()
+        {
+            string phrase = phrases[rnd.Next(0, phrases.Length)];
+            return phrase;
+        }
+
+        public void NewGame()
+        {
+            index = 0;
+            currentPhrase = GetPhrase();
+            words = currentPhrase.Split(' ').ToList();
+            lblWord.Text = currentPhrase;
+        }
+
+        
+        private void checkGame(object sender, KeyEventArgs e)
+        {
+            int indexEnd = words.Count() - 1;
+            if (e.KeyCode == Keys.Space)
+            {
+                if (textBox1.Text.Trim() == words[index])
+                {
+                    correct++;
+                    textBox1.Clear();
+                    if (index < indexEnd)
+                    {
+                        index++;
+                    } else
+                    {
+                        NewGame();
+                    }
+                }
+                else
+                {
+                    incorrect++;
+                }
+                lblRight.Text = $"Correct: {correct}";
+                lblWrong.Text = $"Incorrect: {incorrect}";
+            }
+        }
+    }
+}
